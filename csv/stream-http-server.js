71eca3106c9647294ce343b01130bb7efcd25786
rogeriocassares <roger10cassares@gmail.com>
2023-02-27 22:38:26 -0300

    csv-generate insalledand try to open a csv file from the fs

--- csv/stream-http-server.js
+++ csv/stream-http-server.js
@@ -1,28 +1,37 @@
-import assert from 'assert';
-import { generate } from 'csv-generate';
-import { parse } from 'csv-parse';
+import assert from "assert";
+import { parse } from "csv-parse";
+
+import fs from "node:fsgit";
+const csvPath = new URL("./tasks.csv", import.meta.url);
 
 (async () => {
-  // Initialize the parser by generating random records
-  const parser = generate({
-    high_water_mark: 64 * 64,
-    length: 100
-  }).pipe(
-    parse()
-  );
-  // Intialise count
-  let count = 0;
+    fs.readFile(csvPath, "utf-8")
+      .pipe(parse())
+
   // Report start
-  process.stdout.write('start\n');
+  process.stdout.write("start\n");
   // Iterate through each records
   for await (const record of parser) {
     // Report current line
-    process.stdout.write(`${count++} ${record.join(',')}\n`);
+    process.stdout.write(`${count++} ${record.join(",")}\n`);
     // Fake asynchronous operation
     await new Promise((resolve) => setTimeout(resolve, 100));
   }
   // Report end
-  process.stdout.write('...done\n');
+  process.stdout.write("...done\n");
+
   // Validation
   assert.strictEqual(count, 100);
 })();
+
+// fetch("http://localhost:3334", {
+//   method: "POST",
+//   body: new OneToHundredStream(), // Stream the content the request in the body!
+//   duplex: "half",
+// })
+//   .then((response) => {
+//     return response.text();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
